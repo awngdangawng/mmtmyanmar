9ef33d85766deca80647abd01724a4eae002508a
awngdangawng <126932342+awngdangawng@users.noreply.github.com>
2023-05-18 21:48:23 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceId = "service_tcnk5vr"
+const templateId = "template_wu2sv77"
+
+emailjs.send(serviceId,templateId,params)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message sent successfully");
+    })
+
+.catch((err) => console.log(err));
+
+}
+
